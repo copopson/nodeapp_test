bb329730161d28be1990296b7e8239de9ff707c8
Jenkins <jenkins@ip-172-31-30-21.ec2.internal>
2021-09-12 09:27:55 +0000

    sonare

--- sonar-project.js
+++ sonar-project.js
@@ -3,7 +3,7 @@ const scanner = require('sonarqube-scanner');
 scanner(
   {
     serverUrl : 'http://ec2-50-19-54-175.compute-1.amazonaws.com:9000',
-    token : "58295258a8c2fd6df442bd40626a7eb3efb0acd2",
+    token : "ee6ade24cdb2c625ffb216c43dce347224716030",
     options: {
       'sonar.projectName': 'node-app',
       'sonar.projectDescription': 'Description for "node-app" project...',
